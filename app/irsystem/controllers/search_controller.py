040c691919a52e39e36dce4cac5e8ca0e1ce2e15
Amrit Amar <aa792@cornell.edu>
2020-04-07 16:22:05 -0400

    Update search_controller.py

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Culture for Quarantined Gamers: Anime Recommendations based on Game Preferences"
+net_id = "Amrit Amar (aa792),  Carina Cheng (chc94), Alan Pascual (ap835), Jeffrey Yao (jy398), Wenjia Zhang (wz276)"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -12,7 +12,7 @@ def search():
 		data = []
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
+		output_message = "Your search: " + query + " (this will be replaced with anime)"
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
